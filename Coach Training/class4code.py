4c6b9b47ee19a2331b0ec260c80a26fdd6961087
izz13 <94653804+izz13@users.noreply.github.com>
2024-01-14 14:07:10 -0800

    Update class4code.py

--- Coach Training/class4code.py
+++ Coach Training/class4code.py
@@ -35,6 +35,7 @@ while True:
 
     velocity[1] += gravity*tick 
     
+    #move in the x direction 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
         velocity[0] = speed
@@ -45,6 +46,7 @@ while True:
     
     
     screen.fill([0,0,0])
+    #collision code for bottom of the screen
     if rect1.bottom + velocity[1] >= size[1]:
         dy = size[1] - rect1.bottom
         velocity[1] = 0
@@ -56,31 +58,35 @@ while True:
     dx = velocity[0]
     
 
-   
+   #collision code with platorms
     pygame.draw.rect(screen,[255,0,0],rect1)
     for p in platforms:
+        #if the change in y will hit the platform, change dy to be the distance from the bottom of player and top of platform
+        #change v_y to be zero
         if p.colliderect(pygame.Rect(rect1.x,rect1.y + dy,rect1.w,rect1.h)):
             dy = p.top - rect1.bottom
-            if dy < 0:
-                dy = 0
             velocity[1] = 0
+        #if the change in x will hit the platform and player is moving to the left, change dx to be the distance from the left of player and right of platform
+        #change dx to be zero
         elif p.colliderect(pygame.Rect(rect1.x + dx, rect1.y, rect1.w, rect1.h)) and dx < 0:
             dx = p.right - rect1.left
             velocity[0] = 0
+        #if the change in x will hit the platform and player is moving to the right, change dx to be the distance from the right of player and left of platform
+        #change dx to be zero
         elif p.colliderect(pygame.Rect(rect1.x + dx, rect1.y, rect1.w, rect1.h)) and dx > 0:
             dx = p.left - rect1.right
             velocity[0] = 0
         pygame.draw.rect(screen,[0,0,255],p)
 
     
-
+    #update the player position with dx and dy
     rect1.centerx += dx
     rect1.centery += dy
     pygame.display.update()
     clock.tick(fps)
 
 
-#Ways To Improve
+#Things to Add
 #Add jumping to player
 #stop player from moving when key is let go
 #Have platforms spawn not colliding with eachother
