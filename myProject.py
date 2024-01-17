029df2b3236cbe2efb94e5cb0c45b977f48da444
izz13 <94653804+izz13@users.noreply.github.com>
2024-01-16 18:20:56 -0800

    Added file for rotating image around an object

---
+++ myProject.py
@@ -0,0 +1,71 @@
+import pygame
+
+#set up code
+
+pygame.init()
+#setting up the pygame screen
+WIDTH = 800
+HEIGHT = 640
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+
+#set game title
+pygame.display.set_caption("My Game Title")
+
+#set up the pygame clock
+clock = pygame.time.Clock()
+fps = 60
+
+#set up colors
+white = [255,255,255]
+black = [0,0,0]
+yellow = [255,255,0]
+
+#set up functions for game objects
+#each game object should have 3 functions
+#1. first function is the create function
+#this should create a dictionary that defines the game object
+#2. second function is the set function
+#this should set the keys in the game object dictionary to values
+#3. the third function is the update function
+#This should tell pygame what the game object does each frame
+
+#this is the player object
+def createPlayer():
+    player = {
+        "x" : 0,
+        "y" : 0,
+        "width" : 0,
+        "height" : 0,
+        "rect" : pygame.Rect(0,0,0,0),
+        "color" : yellow
+    }
+    return player
+
+def setPlayer(player):
+    player["x"] = WIDTH/2
+    player["y"] = HEIGHT/2
+    player["width"] = 50
+    player["height"] = 50
+    player["rect"].center = [player["x"],player["y"]]
+    player["rect"].width = player["width"]
+    player["rect"].height = player["height"]
+
+def updatePlayer(player):
+    player["rect"].center = [player["x"],player["y"]]
+    pygame.draw.rect(screen,player["color"],player["rect"])
+
+#creating player object from the player functions
+player = createPlayer()
+setPlayer(player)
+
+#GameLoop
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    
+    screen.fill(white)
+    updatePlayer(player)
+    pygame.display.update()
+    clock.tick(fps)
---
+++ rotate_around.py
@@ -0,0 +1,101 @@
+import pygame
+from pygame.math import Vector2
+import math
+
+pygame.init()
+WIDTH = 500
+HEIGHT = 500
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+clock = pygame.time.Clock()
+fps = 60
+
+class Player:
+    def __init__(self,x,y,w,h):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(x,y,w,h)
+        self.speed = 5
+
+    def move(self,keys):
+        moving = False
+        vel = Vector2(0)
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            vel[0] = self.speed
+            moving = True
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            vel[0] = -self.speed
+            moving = True
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            vel[1] = -self.speed
+            moving = True
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            vel[1] = self.speed
+            moving = True
+        if not moving:
+            vel = Vector2(0)
+        if vel != Vector2(0):
+            vel = vel.normalize()
+        vel[0] *= self.speed
+        vel[1] *= self.speed
+        self.x += vel[0]
+        self.y += vel[1]
+
+        
+
+    def render(self,screen):
+        self.rect.center = [self.x,self.y]
+        #pygame.draw.rect(screen,[255,0,0],self.rect)
+        pygame.draw.circle(screen,[255,0,0],[self.x,self.y],self.rect.w)
+
+    def update(self,screen,keys):
+        self.move(keys)
+        self.render(screen)
+
+class Dagger:
+    def __init__(self,x,y,w,h,image):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(x,y,w,h)
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image,[self.rect.w,self.rect.h])
+
+    def render(self,screen,player,mPos):
+        self.rect.center = [self.x,self.y]
+        angle = math.atan2(mPos[1]-player.y,mPos[0]- player.x)*180/math.pi + 90
+        image_rot = pygame.transform.rotate(self.image,angle*-1)
+        image_rect = image_rot.get_rect()
+        image_rect.center= [self.x,self.y]
+        #pygame.draw.rect(screen,[0,0,255],image_rect)
+        screen.blit(image_rot,image_rect)
+
+    def move(self,player,mPos):
+        dist_from_player = 100
+        mPos = Vector2(mPos)
+        pos = mPos - Vector2(player.x,player.y)
+        pos.scale_to_length(dist_from_player)
+        self.x = player.x + pos[0]
+        self.y = player.y + pos[1]
+
+
+    def update(self,screen,mPos,player):
+        self.render(screen,player,mPos)
+        self.move(player,mPos)
+
+player = Player(WIDTH/2,HEIGHT/2,36,36)
+dagger = Dagger(WIDTH/2,HEIGHT/2,16,32,"dagger.png")
+
+while True:
+    mPos = pygame.mouse.get_pos()
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    keys = pygame.key.get_pressed()
+    screen.fill([0,0,0])
+    player.update(screen,keys)
+    dagger.update(screen,mPos,player)
+    pygame.display.update()
+    clock.tick(fps)
+
+
+
